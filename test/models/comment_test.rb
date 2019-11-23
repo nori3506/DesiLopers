148d35836d09b39d12d39ac5bc9b5e79656556d6
Nori <loveandfight2186@yahoo.co.jp>
2019-11-24 02:47:33 +0900

    annotation追加

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id           :bigint           not null, primary key
+#  content      :text(65535)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  portfolio_id :bigint
+#  user_id      :integer
+#
+# Indexes
+#
+#  index_comments_on_portfolio_id  (portfolio_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (portfolio_id => portfolios.id)
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
