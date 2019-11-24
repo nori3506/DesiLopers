83ac7d37ffca7d267b2dc32e34798e9fdcd0c5d2
Nori <loveandfight2186@yahoo.co.jp>
2019-11-25 00:16:00 +0900

    dockeryml change　and config change for factorybot

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,9 @@ module Desilopers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
     config.generators do |g|
       g.test_framework :rspec,
-      fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
