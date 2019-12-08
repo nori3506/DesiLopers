1973fa30e636b6d33758044a14c5e2ae1415ba7c
Nori <loveandfight2186@yahoo.co.jp>
2019-12-09 00:34:34 +0900

    userspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: users
@@ -21,28 +23,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #名前、メール、パスワードがあれば有効な状態であること
-  it "is valid with name, email, password" do
+  # valid with name, email, and password
+  it 'is valid with name, email, password' do
     user = User.new(
-      name:       "Aaron",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      name: 'Aaron',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze'
     )
     expect(user).to be_valid
   end
-  # 名がなければ無効な状態であること
-  it "is invalid without a name" do
+  # invalid without name
+  it 'is invalid without a name' do
     user = User.new(name: nil)
     user.valid?
     expect(user.errors[:name]).to_not include("can't be blank")
   end
-  # 姓がなければ無効な状態であること
-  it "is invalid without a last name"
-  # メールアトレスがなければ無効な状態であること
-  it "is invalid without an email address"
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
-  # ユーザーのフルネームを文字列として返すこと
-  it "returns a user's full name as a string"
-
 end
