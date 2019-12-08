ad35e263db2de417f41f0d4dc9652cf809bf9b6a
Nori <loveandfight2186@yahoo.co.jp>
2019-12-09 00:05:55 +0900

    settings.josn とfactory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-   name { 'nori' }
-   age { "20" }
-   area { "Canada" }
-   career_year { "30years" }
-   email { "nori@nori.nori" }
-   gender { "male" }
-   hobby { "football" }
-	image { "" }
-	job_hunting { true }
-	password { "password" }
+    name { 'nori' }
+    age { '20' }
+    area { 'Canada' }
+    career_year { '30years' }
+    email { 'nori@nori.nori' }
+    gender { 'male' }
+    hobby { 'football' }
+    image { '' }
+    job_hunting { true }
+    password { 'password' }
   end
 end
