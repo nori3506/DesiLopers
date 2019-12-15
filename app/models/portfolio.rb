3929d9eedc39b213b53bea313daab67ee368cf42
Nori <loveandfight2186@yahoo.co.jp>
2019-12-15 19:59:21 +0900

    validation modi

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -29,14 +29,14 @@ class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :comments
   default_scope -> { order(created_at: :desc) }
-  validates :first_site,presence:true, format: /\A#{URI::regexp(%w(http https))}\z/
+  validates :first_site, presence:true, format: /\A#{ URI::regexp(%w(http https)) }\z/
   mount_uploader :first_image, ImageUploader
   validates :second_site, format: /\A#{URI::regexp(%w(http https))}\z/, if: :second_site?
   mount_uploader :second_image, ImageUploader
-  validates :third_site, format: /\A#{URI::regexp(%w(http https))}\z/, if: :third_site?
+  validates :third_site, format: /\A#{ URI::regexp(%w(http https)) }\z/, if: :third_site?
   mount_uploader :third_image, ImageUploader
   validates :career, presence: true
-  validates :experience, presence: true, length: {minimum:5}
-  validates :message, presence: true, length:{minimum:5}
-
+  min_number = 5
+  validates :experience, presence: true, length: { minimum: 5, too_short: "最大#{ min_number }文字以上入力してください。" }
+  validates :message, presence: true, length: { minimum: 5, too_short: "最大#{ min_number }文字以上入力してください。"  }
 end
