0aaacc90bacab2e7e3545eec0802421e7ceadad8
Nori <loveandfight2186@yahoo.co.jp>
2020-01-13 19:16:47 +0900

    devise　user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,47 @@
 #
 # Table name: users
 #
-#  id                :bigint           not null, primary key
-#  admin             :boolean          default(FALSE), not null
-#  age               :string(255)
-#  area              :string(255)
-#  birthday(誕生日)  :date
-#  career_year       :string(255)
-#  email             :string(255)
-#  gender            :string(255)
-#  hobby             :string(255)
-#  image             :string(255)
-#  job_hunting       :boolean
-#  name              :string(255)
-#  password_digest   :string(255)
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
+#  id                       :bigint           not null, primary key
+#  admin                    :boolean          default(FALSE), not null
+#  age                      :string(255)
+#  area                     :string(255)
+#  birthday(誕生日)         :date
+#  career_year              :string(255)
+#  confirmation_sent_at     :datetime
+#  confirmation_token       :string(255)
+#  confirmed_at             :datetime
+#  current_sign_in_at       :datetime
+#  current_sign_in_ip       :string(255)
+#  email                    :string(255)
+#  encrypted_password       :string(255)      default(""), not null
+#  gender                   :string(255)
+#  hobby                    :string(255)
+#  image                    :string(255)
+#  job_hunting              :boolean
+#  last_sign_in_at          :datetime
+#  last_sign_in_ip          :string(255)
+#  name                     :string(255)
+#  password_digest          :string(255)
+#  remember_created_at      :datetime
+#  reset_password_sent_at   :datetime
+#  reset_password_token     :string(255)
+#  sign_in_count            :integer          default(0), not null
+#  unconfirmed_email        :string(255)
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable
   before_save {self.email = email.downcase}
   default_scope -> { order(created_at: :desc) }
   validates :password, presence:true, length:{minimum: 6}, allow_nil: true
