81839077c0680d7d47d41edb344275574e8ac830
Nori <loveandfight2186@yahoo.co.jp>
2020-04-26 16:55:58 +0900

    remove alert

--- app/assets/javascripts/teches.js
+++ app/assets/javascripts/teches.js
@@ -1,14 +1,11 @@
 $(function(){
 	function buildHtml(tech){
-		
 		var html = `<td><a href = /teches/${tech.id}>${tech.name}</a></td>`
 		return	html;
 	}	
 	$('#tech_form').on('submit', function(e) {
 		e.preventDefault();
-		alert("agd");
 		var formData = new FormData(this);
-		alert(formData);
 		$.ajax({
 			url: "/teches",
 			type: "POST",
@@ -18,10 +15,7 @@ $(function(){
 			contentType: false
 		})
 		.done(function(data){
-			alert("done");
-			alert(buildHtml)
 			var html = buildHtml(data);
-			alert(html)
 			$('.tech_list').append(html)
 			$('.tech_text').val('')
 		})
