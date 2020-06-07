144527f3403312db913ca31998b78064773f51b2
Nori <loveandfight2186@yahoo.co.jp>
2020-06-07 22:02:06 +0900

    create user_jobs table

--- app/models/job_category.rb
+++ app/models/job_category.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: job_categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)
+#  sequence   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class JobCategory < ApplicationRecord
 
 end
