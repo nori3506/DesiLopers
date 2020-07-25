b2aecef91486963752dd2a5f527098714df5abef
Nori <loveandfight2186@yahoo.co.jp>
2020-07-26 00:18:22 +0900

    initial pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SessionsHelper
+  include Pundit
 
   def after_sign_in_path_for(resource)    
     if resource.class == AdminUser
