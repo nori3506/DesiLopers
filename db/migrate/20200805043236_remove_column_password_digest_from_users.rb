69f8b89694bb132869e01692ed997821b52042ee
Nori <loveandfight2186@yahoo.co.jp>
2020-08-05 13:46:00 +0900

    delete password digest column

---
+++ db/migrate/20200805043236_remove_column_password_digest_from_users.rb
@@ -0,0 +1,9 @@
+class RemoveColumnPasswordDigestFromUsers < ActiveRecord::Migration[6.0]
+  def up
+    remove_column :users, :password_digest
+  end
+
+  def down
+    add_column :users, :password_digest, :string
+  end
+end
