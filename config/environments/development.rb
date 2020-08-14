96bf010219ccec9ac6f6754b7a6a05fda2b1ebf7
Nori <loveandfight2186@yahoo.co.jp>
2020-08-14 22:28:24 +0900

    add new whitelist in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::FileUpdateChecker
   config.web_console.whitelisted_ips = '172.22.0.1'
-  config.web_console.whitelisted_ips = '172.20.0.1'
+	config.web_console.whitelisted_ips = '172.20.0.1'
+	config.web_console.whitelisted_ips = '172.18.0.1'
+	
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 
   config.after_initialize do
