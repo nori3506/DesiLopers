93a416189f6ac5d6081d3658351d28a60d40f31e
Nori <loveandfight2186@yahoo.co.jp>
2020-08-31 23:41:04 +0900

    indent

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,11 @@
 class ProjectsController < ApplicationController
-	def index
-		@projects = Project.active_projects
-	end
+  def index
+    @projects = Project.active_projects
+  end
 
-	def show
-		@project = Project.find(params[:id])
-		# todo: other projects list @other_projects shoud be defined
-	end
-	
+  def show
+    @project = Project.find(params[:id])
+    # todo: other projects list @other_projects shoud be defined
+  end
+  
 end
