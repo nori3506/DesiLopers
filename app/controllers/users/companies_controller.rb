f29af6428f697529bfe2d30a17d45a0207607b9e
Nori <loveandfight2186@yahoo.co.jp>
2020-10-29 10:44:36 +0900

    create show action in users/companies_controller

--- app/controllers/users/companies_controller.rb
+++ app/controllers/users/companies_controller.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class Users::CompaniesController < ApplicationController
+	before_action :set_company, only: [:show]
+
+	def show
+	
+	end
+
+	 private
+
+	def set_company
+		@company = Company.find(params[:id])
+	end
 end
