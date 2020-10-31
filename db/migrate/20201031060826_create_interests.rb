fb88e95f8d92ff32ffa2ce00fe80db4a3040816f
Nori <loveandfight2186@yahoo.co.jp>
2020-10-31 16:14:45 +0900

    implement migration file for interests table

--- db/migrate/20201031060826_create_interests.rb
+++ db/migrate/20201031060826_create_interests.rb
@@ -1,7 +1,10 @@
 class CreateInterests < ActiveRecord::Migration[6.0]
   def change
     create_table :interests do |t|
-
+      t.references :user, foreign_key: true, nil: false
+      t.references :project, foreign_key: true, nil: false
+      t.boolean :is_interest_by_user, default: true, comment: "interest by user"
+      t.index ["user_id", "project_id"], unique: true
       t.timestamps
     end
   end
