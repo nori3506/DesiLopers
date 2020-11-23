cb54d785972f63c8f636d1afe5c6fdf01ebb78fc
Nori <loveandfight2186@yahoo.co.jp>
2020-11-23 18:22:14 +0900

    handle nil error in project_decorator

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -2,6 +2,6 @@ class ProjectDecorator < Draper::Decorator
   delegate_all
 
   def interest_project?
-    helpers.current_user.projects.interest_projects.include?(object)
+    helpers.current_user&.projects&.interest_projects&.include?(object)
   end
 end
