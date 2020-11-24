50f8e87880ec0c7862d14520d36686ae8c2b6731
Nori <loveandfight2186@yahoo.co.jp>
2020-11-24 23:58:38 +0900

    add humburger menu JS code in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,3 +64,15 @@ $(function() {
   };
 
 })(jQuery);
+
+$(function () {
+  $('.hamburger').click(function () {
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) {
+      $('.globalMenuSp').addClass('active');
+    } else {
+      $('.globalMenuSp').removeClass('active');
+    }
+  });
+});
