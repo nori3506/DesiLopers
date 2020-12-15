e1b6fbcf0bd34e6e36cbeb164a62ed4c7b08f775
Nori <loveandfight2186@yahoo.co.jp>
2020-12-15 16:18:44 +0900

    when send notification, user_id set

--- app/models/modules/notification.rb
+++ app/models/modules/notification.rb
@@ -1,15 +1,15 @@
 module Modules::Notification
 	class << self
 		def notice_company_user_interest(user, project)
-			Notifications::Company.create!(title: "#{user.name} is interested in the job #{project.title} ", source_id: project.company.id)
+			Notifications::Company.create!(title: "#{user.name} is interested in the job #{project.title} ", source_id: project.company.id, user_id: user.id)
 		end
 
 		def notice_company_user_apply(user, project)
-			Notifications::Company.create!(title: "#{user.name} Applied the job #{project.title}", source_id: project.company.id)
+			Notifications::Company.create!(title: "#{user.name} Applied the job #{project.title}", source_id: project.company.id, user_id: user.id)
 		end
 
 		def notice_user_company_interest(user,project)
-			Notifications::Company.create!(title: "#{company.name} has interest", source_id: project.company.id)
+			Notifications::Company.create!(title: "#{company.name} has interest", source_id: project.company.id, user_id: user.id)
 		end
 	end
 end
