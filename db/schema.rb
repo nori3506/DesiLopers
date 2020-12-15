7cc5bc6e5fe21ff79dcdf9166f6ccedb79776d98
Nori <loveandfight2186@yahoo.co.jp>
2020-12-15 22:07:48 +0900

    create channel table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_065131) do
+ActiveRecord::Schema.define(version: 2020_12_15_125128) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
@@ -49,6 +49,11 @@ ActiveRecord::Schema.define(version: 2020_12_15_065131) do
     t.integer "salary"
     t.text "url"
     t.index ["user_id"], name: "index_careers_on_user_id"
+  end
+
+  create_table "channels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
---
+++ spec/factories/channels.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: channels
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :channel do
+    
+  end
+end
