7215ed13deeba69cb2e2c54b1aa949be0c9e31f8
Nori <loveandfight2186@yahoo.co.jp>
2020-12-17 00:06:44 +0900

    modify indentation

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,19 +1,20 @@
 class Api::MessagesController < ApplicationController
-	def create
-		Channel.transaction do
-			@channel = Channel.create
-			Channels::User.create(channel_id: @channel.id, source_id: message_params[:user_id])
-			Channels::Company.create(channel_id: @channel.id, source_id: message_params[:company_id])
-			Messages::Company.create(channel_id: @channel.id, source_id: message_params[:company_id], content: message_params[:content])
-		rescue => e
-			flash[:alert] = "Sorry, Message could not be sent"
-			@user = User.find(message_params[:user_id])
-			render "companies/users/show"
-		end
-	end
+  def create
+    Channel.transaction do
+      @channel = Channel.create
+      Channels::User.create(channel_id: @channel.id, source_id: message_params[:user_id])
+      Channels::Company.create(channel_id: @channel.id, source_id: message_params[:company_id])
+      Messages::Company.create(channel_id: @channel.id, source_id: message_params[:company_id], content: message_params[:content])
+    rescue => e
+      flash[:alert] = "Sorry, Message could not be sent"
+      @user = User.find(message_params[:user_id])
+      render "companies/users/show"
+    end
+  end
 
-	private
-	def message_params
-		params.permit(:content, :company_id, :user_id)
-	end
+  private
+
+  def message_params
+    params.permit(:content, :company_id, :user_id)
+  end
 end
