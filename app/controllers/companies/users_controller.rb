84bdcdd969b0fd0ca38f7b149fb87ae02d7b81b8
Nori <loveandfight2186@yahoo.co.jp>
2020-12-16 23:59:55 +0900

    implement channel

--- app/controllers/companies/users_controller.rb
+++ app/controllers/companies/users_controller.rb
@@ -2,6 +2,10 @@ class Companies::UsersController < Companies::ApplicationController
 	before_action :find_user, only: [:show]
 
 	def show
+		user_channel = Channels::User.includes(:channel).where(source_id: @user.id)&.map(&:channel)
+		company_channel = Channels::Company.includes(:channel).where(source_id: current_user.company.id)&.map(&:channel)
+		@is_channel = (user_channel & company_channel).present?
+
 	end
 
 	private
