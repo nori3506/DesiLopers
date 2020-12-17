df53336d6d48e0c5ab112723e86022c2b4c45c15
Nori <loveandfight2186@yahoo.co.jp>
2020-12-17 23:59:45 +0900

    create channels_controller and after create comment, link to there for company

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,14 +1,13 @@
 class Api::MessagesController < ApplicationController
   def create
-    Channel.transaction do
+    Channel.transaction do      
       @channel = Channel.create
       Channels::User.create(channel_id: @channel.id, source_id: message_params[:user_id])
       Channels::Company.create(channel_id: @channel.id, source_id: message_params[:company_id])
       Messages::Company.create(channel_id: @channel.id, source_id: message_params[:company_id], content: message_params[:content])
       flash[:success] = "Your message was successfully sent"
-      
       # redirect link will be changed to message_show page
-      redirect_to companies_candidate_path(message_params[:user_id])
+      redirect_to companies_channel_path(@channel)
     rescue => e
       flash[:alert] = "Sorry, Message could not be sent"
       @user = User.find(message_params[:user_id])
