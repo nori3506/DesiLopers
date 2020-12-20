25f73b4735549fce9d8f8ff35212ef1ab67afbb9
Nori <loveandfight2186@yahoo.co.jp>
2020-12-21 00:36:59 +0900

    modify method name of project's type

--- app/controllers/companies/home_controller.rb
+++ app/controllers/companies/home_controller.rb
@@ -11,20 +11,20 @@ class Companies::HomeController < Companies::ApplicationController
     @stop_projects = @projects.stop_projects
     @stop_projects_number = @projects.stop_projects.size
     @notifications = Notifications::Company.my_notifications(company).order(created_at: :desc)
-    @screening_users = User.screening_users(company)
+    @more_than_five_notifications = @notifications&.size > 5
+    @screening_users = User.candidates_users(company, "screening")
     @screening_users_number = @screening_users.size
-    @skill_check_users = User.skill_check_users(company)
+    @skill_check_users = User.candidates_users(company, "skill_check")
     @skill_check_users_number = @skill_check_users.size
-    @interview_users = User.interview_users(company)
+    @interview_users = User.candidates_users(company, "interview")
     @interview_users_number = @interview_users.size
-    @recruitment_offer_users = User.recruitment_offer_users(company)
+    @recruitment_offer_users =User.candidates_users(company, "recruitment_offer")
     @recruitment_offer_users_number = @recruitment_offer_users.size
-    @signed_users = User.signed_users(company)
+    @signed_users = User.candidates_users(company, "signed")
     @signed_users_number = @signed_users.size
-    @reject_users = User.rejected_users(company)
+    @reject_users = User.candidates_users(company, "reject")
     @reject_users_number = @reject_users.size
   end
-
   private
 
   def authorize_home
