3d3cf53dc5cc9ca6820d95e750b1809d69312abd
Nori <loveandfight2186@yahoo.co.jp>
2020-12-20 17:35:42 +0900

    add link to Open Message button in company/user show page

--- app/controllers/companies/users_controller.rb
+++ app/controllers/companies/users_controller.rb
@@ -5,6 +5,7 @@ class Companies::UsersController < Companies::ApplicationController
     user_channel = Channel.includes(:channel_users).where('channel_users.user_id' => @user.id)
     company_channel = Channel.includes(:channel_users).where('channel_users.company_id' => current_user.company.id)
     @is_channel = (user_channel & company_channel).present?
+    @channel = (user_channel & company_channel)&.first
   end
 
   private
