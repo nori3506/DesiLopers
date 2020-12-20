7e659c700883ebc4cc76d7e1ac0e6d16d9f98fc5
Nori <loveandfight2186@yahoo.co.jp>
2020-12-21 00:50:54 +0900

    add desc order  at notifications controller index action

--- app/controllers/companies/notifications_controller.rb
+++ app/controllers/companies/notifications_controller.rb
@@ -1,7 +1,7 @@
 class Companies::NotificationsController < Companies::ApplicationController
 
 	def index
-		@notifications = Notification.all
+		@notifications = Notification.all.order(created_at: :desc)
 	end
 
 end
