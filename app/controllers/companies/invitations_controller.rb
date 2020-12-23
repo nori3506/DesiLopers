aadd8a9dce0db49a1b2c3e47fb097a9db052911c
Nori <loveandfight2186@yahoo.co.jp>
2020-12-24 00:08:36 +0900

    indentation modify

--- app/controllers/companies/invitations_controller.rb
+++ app/controllers/companies/invitations_controller.rb
@@ -1,19 +1,19 @@
 class Companies::InvitationsController < Devise::InvitationsController
-	before_action :configure_permitted_parameters, if: :devise_controller?
-	layout 'layouts/company/layout.html.erb', only: :new
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout 'layouts/company/layout.html.erb', only: :new
 
-	def new
-		super
-	end
-	
-	def create
-		super
-		resource.update_columns(company_id: current_user.company.id)
-	end
+  def new
+    super
+  end
+  
+  def create
+    super
+    resource.update_columns(company_id: current_user.company.id)
+  end
 
-	def edit
-		super
-	end
+  def edit
+    super
+  end
 
 protected
 
@@ -22,13 +22,13 @@ protected
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
   end
 
-	private
-	def after_invite_path_for(inviter, invitee)
-		new_user_invitation_path
-	end
+  private
+  def after_invite_path_for(inviter, invitee)
+    new_user_invitation_path
+  end
 
-	def after_accept_path_for(resource)
-		companies_home_index_path
-	end
+  def after_accept_path_for(resource)
+    companies_home_index_path
+  end
 
 end
