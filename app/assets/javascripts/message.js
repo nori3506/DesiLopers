d5c31572791d64dd3ecc83b9069cae72a6f8f70a
Nori <loveandfight2186@yahoo.co.jp>
2021-05-04 02:06:06 -0700

    create comment function in channels page

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,41 @@
+$(function () {
+
+	function displayMessages(data) {
+		let messages = []
+			if (!data.message.user_id) {
+			messages.push(`<div class="messageFromCompany"><p>${data.message.content}</p></div>`)
+			} else {
+			messages.push(`<div class="messageFromUser"><p>${data.message.content}</p></div>`)
+			}
+		return messages
+  }
+  
+	const submitBtn = document.getElementById('messageSendBtn');
+	submitBtn.addEventListener('click', (e) => {
+		const messageForm = document.getElementById('messageForm');
+		e.preventDefault();
+		submitBtn.textContent = 'Sending...';
+		submitBtn.disabled = true;
+    let formData = new FormData(messageForm);
+
+    $.ajax({
+      url: `/api/messages`,
+      type: "post",
+      data: formData,
+			dataType: 'json',
+	    processData: false,
+      contentType: false
+    })
+		.done(function (data) {
+			$('#messageTextArea').val("");
+			submitBtn.textContent = 'Send';
+			submitBtn.disabled = false;
+
+      $('#messageDisplay').append(displayMessages(data))
+    })
+    .fail(function (data) {
+      alert('something went wrong, sorry')
+      // jsInterestByUser.innerHTML = "Interest!"
+    })
+  })
+});
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,30 +1,25 @@
 class Api::MessagesController < ApplicationController
   def create
-    # userかcompanyかの判別をする
-    channel = Channel.find(params[:channel_id])
+    channel = Channel.find(params[:channel_id])    
     if current_user.company_user?
-      if Modules::Message::create_message_from_company(channel, params[:company_id], params[:content])
-        flash[:success] = "Your message was successfully sent"
-        if current_user.company_user?
-          redirect_to companies_channel_path(channel)
-        else
-          redirect_to channel_path(channel)
+      if @message = Modules::Message::create_message_from_company(channel, params[:company_id], params[:content])
+        respond_to do |format|
+          format.html { redirect_to :channels, notice: 'Message was successfully sent' }
+          format.json
         end
+        flash[:success] = "Your message was successfully sent"
       else
         flash[:alert] = "Sorry, Message could not be sent"
-        render "companies/channels/show"
       end
-    else
-      if Modules::Message::create_message_from_user(channel, params[:user_id], params[:content])
-        flash[:success] = "Your message was successfully sent"
-        if current_user.company_user?
-          redirect_to companies_channel_path(channel)
-        else
-          redirect_to channel_path(channel)
+    else      
+      if @message = Modules::Message::create_message_from_user(channel, params[:user_id], params[:content])
+        respond_to do |format|
+          format.html { redirect_to :channels, notice: 'Message was successfully sent' }
+          format.json
         end
+        flash[:success] = "Your message was successfully sent"        
       else
         flash[:alert] = "Sorry, Message could not be sent"
-        render "companies/channels/show"
       end
     end
     
