173fcc34d51c9c8fe9f73816eecdde83b14e64bc
Nori <loveandfight2186@yahoo.co.jp>
2021-12-15 17:50:57 -0800

    add hover overlay in company header and modify button padding

--- app/javascript/components/home/Description.jsx
+++ app/javascript/components/home/Description.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Description = () => (
-	<React.Fragment>
-		<div class="home-left-side">
-			<div class="home-desctiption">
-				<h1 class="welcome">DesiLopers<br /> for your career</h1>
-				<div class="home-text">
-					<p class="home-first-text-area">
-						<span class="u-text-yellow">Desi</span>gners + Deve<span class="u-text-yellow">lopers</span> <br /> = <br />
-						<span class="u-text-yellow stronger">"DesiLopers"</span>
-					</p>
-					<p class="home-explanation u-pt20">
-						DesiLopers enables all Designers and Developers can search the best job offer.
-					</p>
-				</div>
-			</div>
-			<div class="home-main-image">
-			</div>
-		</div >
-	</React.Fragment>
-)
+  <React.Fragment>
+    <div class="home-left-side">
+      <div class="home-desctiption">
+        <h1 class="welcome">
+          DesiLopers
+          <br /> for your career
+        </h1>
+        <div class="home-text">
+          <p class="home-first-text-area">
+            <span class="u-text-yellow">Desi</span>gners + Deve
+            <span class="u-text-yellow">lopers</span> <br /> = <br />
+            <span class="u-text-yellow stronger">"DesiLopers"</span>
+          </p>
+          <p class="home-explanation u-pt20">
+            DesiLopers enables all Designers and Developers can search the best
+            job offer.
+          </p>
+        </div>
+      </div>
+      <div class="home-main-image"></div>
+    </div>
+  </React.Fragment>
+);
 
-export default Description
+export default Description;
